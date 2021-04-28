5745991d2880ba99c5a56a8ff3bd262cd70bb5ec
NicklausDim <nikolas.dimitriadis@hotmail.com>
2021-04-28 23:56:11 +0200

    last version

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Social extends Component {
     render() {
         return (
-            <div class="social">
+            <div className="social">
             <a href="https://codepen.io/nicklaus-dimitriadis" target="_blank" rel="noopener noreferrer"><i class="fab fa-codepen"></i></a>
             <a href="https://github.com/NicklausDim" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
             <a href="https://www.instagram.com/call_me_nicklaus/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
 
 import "./Skills.css";
+import bootstrapImage from "../img/BootstrapImage.svg";
+import cssImage from "../img/CssImage.svg";
+import figmaImage from "../img/FigmaImage.svg";
+import htmlImage from "../img/HTMLImage.svg";
+import jsImage from "../img/JSImage.svg";
+import photoshopImage from "../img/PhotoshopImage.svg";
+import reactImage from "../img/react.svg";
+import sassImage from "../img/SassImage.svg";
+import vsImage from "../img/VisualStudioImage.svg";
+import windowsImage from "../img/WindowsImage.svg";
+import React from 'react'
+    
+    const Skills = () => {
+        const skillsIcons = [
+            bootstrapImage,
+            cssImage ,
+            figmaImage, 
+            htmlImage ,
+            jsImage ,
+            photoshopImage,
+            reactImage,
+            sassImage,
+            vsImage ,
+            windowsImage 
 
-
-
-
-class Skills extends Component {
-
-    render() {
+        ];
+        const allSkills=skillsIcons.map(icon => <div className="grid-items" style={{backgroundImage:`url(${icon})`}}></div> )
         return (
             <div className="condiv skills">
                 <h1 className="subtopic">My Skills</h1>
                 <div className="grid-container">
-                    <div className="grid-items">
-                    </div>
-                    <div className="grid-items">
-                    </div>
-                    <div className="grid-items">
-                    </div>
-                    <div className="grid-items">
-                    </div>
-                    <div className="grid-items">
-                    </div>
-                    <div className="grid-items">
-                    </div>
-                    <div className="grid-items">
-                    </div>
-                    <div className="grid-items">
-                    </div>
-                    <div className="grid-items">
-                    </div>
-                    <div className="grid-items">
-                    </div>
+
+                {allSkills}
                 </div>
             </div>
-            )
-        }
+        )
     }
+        
     
     export default Skills
     
